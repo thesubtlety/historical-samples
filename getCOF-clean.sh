0252236d63dd932bbbf34dc9e76c63b09ce1dac5
np <noah@thesubtlety.com>
2012-11-20 17:41:33 -0600

    Clean up

--- getCOF-clean.sh
+++ getCOF-clean.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-#This script will yahoo finance every
-#so often and upon recieving a suitable stock
-#price, email me.
+#***********************************************************
+# getCOF.sh - thesubtlety - Nov 20, 2012
+# 
+# This script polls yahoo finance every
+# so often (3 hours) and upon recieving a suitable stock
+# price, emails me. Very hacky and no robustness..
+#
+# TO DO
+# >add functionality to search for stock name
+# >replace COF with user input
+# >search the site for the spand id='xyz' that is in grep below
+# >search the <h2> tag for the full name
+#
+#**********************************************************
 
-#add functionality to search for stock name
-#replace COF with user input
-#search the site for the spand id='xyz' that is in grep below
-#search the <h2> tag for the full name
-
-url='finance.yahoo.com/q?s=COF&ql=1'
+URL='finance.yahoo.com/q?s=COF&ql=1'
 DESIRED_PRICE=60
 STOCK_PRICE=0
-WAIT_TIME=300
+WAIT_TIME=10800
 
 while [ true ]; do
 
 DATE=$(date)
 HOUR=$(date +%H)
 
-  if [[ $HOUR -eq 10 ||  $HOUR -eq 15 ]]; then
-
-  stock_price="$(lynx -source $url | grep -o 'yfs_l84_cof.......' | sed 's/yfs_l84_cof">//' )"
-  echo $STOCK_PRICE > /tmp/stock_price
-
-  echo
-  echo $date
-  echo "Capital One Financial Corp. (COF)"
-  echo "Stock Price: " $STOCK_PRICE
-
-  if  [[ ${STOCK_PRICE%%.*} -gt ${DESIRED_PRICE%%.*} ]] ; then 
-      thunderbird -compose "to='email',subject='COF Price',body='The COF Stock Price is now $(cat /tmp/stock_price)'" 
-      sleep 2
-      xdotool search --name "Write: COF Price" windowactivate key ctrl+Return
-      exit 1;
-  fi
-
-fi
+# Pulls down the source from the COF finance page. the yfs_l84 seems to be unique for the 
+# stock price. I'm assuming each stock has its own id
+STOCK_PRICE="$(lynx -source $URL | grep -o 'yfs_l84_cof.......' | sed 's/yfs_l84_cof">//' )"
+echo $STOCK_PRICE > /tmp/stock_price
 
 echo
+echo $date
+echo "Capital One Financial Corp. (COF)"
+echo "Stock Price: " $STOCK_PRICE
+
+# Once the price is higher than I specify, emails me
+if  [[ ${STOCK_PRICE%%.*} -gt ${DESIRED_PRICE%%.*} ]] ; then 
+    thunderbird -compose "to='email',subject='COF Price',body='The COF Stock Price is now $(cat /tmp/stock_price)'" 
+    sleep 2
+    xdotool search --name "Write: COF Price" windowactivate key ctrl+Return
+    exit 1;
+fi
 
 sleep $WAIT_TIME
 done
