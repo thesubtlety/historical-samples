c557f65df4fcaaa097577e5587f08838618e6d55
np <noah@thesubtlety.com>
2012-11-19 21:55:27 -0600

    Changed check times

--- getCOF-clean.sh
+++ getCOF-clean.sh
@@ -3,38 +3,43 @@
 #so often and upon recieving a suitable stock
 #price, email me.
 
-#TO DO
 #add functionality to search for stock name
 #replace COF with user input
 #search the site for the spand id='xyz' that is in grep below
 #search the <h2> tag for the full name
 
 url='finance.yahoo.com/q?s=COF&ql=1'
-desired_price=60
-stock_price=0
-wait_time=21600
-date=$(date)
+DESIRED_PRICE=60
+STOCK_PRICE=0
+WAIT_TIME=300
 
 while [ true ]; do
 
-stock_price="$(lynx -source $url | grep -o 'yfs_l84_cof.......' | sed 's/yfs_l84_cof">//' )"
-echo $stock_price > /tmp/stock_price
+DATE=$(date)
+HOUR=$(date +%H)
 
-echo
-echo $date
-echo "Capital One Financial Corp. (COF)"
-echo "Stock Price: " $stock_price
+  if [[ $HOUR -eq 10 ||  $HOUR -eq 15 ]]; then
 
-if  [[ ${stock_price%%.*} -gt ${desired_price%%.*} ]] ; then 
-    thunderbird -compose "to='email-address',subject='COF Price',body='The COF Stock Price is now $(cat /tmp/stock_price)'" 
-    sleep 2
-    xdotool search --name "Write: COF Price" windowactivate key ctrl+Return
-    exit 1;
+  stock_price="$(lynx -source $url | grep -o 'yfs_l84_cof.......' | sed 's/yfs_l84_cof">//' )"
+  echo $STOCK_PRICE > /tmp/stock_price
+
+  echo
+  echo $date
+  echo "Capital One Financial Corp. (COF)"
+  echo "Stock Price: " $STOCK_PRICE
+
+  if  [[ ${STOCK_PRICE%%.*} -gt ${DESIRED_PRICE%%.*} ]] ; then 
+      thunderbird -compose "to='email',subject='COF Price',body='The COF Stock Price is now $(cat /tmp/stock_price)'" 
+      sleep 2
+      xdotool search --name "Write: COF Price" windowactivate key ctrl+Return
+      exit 1;
+  fi
+
 fi
 
 echo
 
-sleep $wait_time
+sleep $WAIT_TIME
 done
 
 exit 0
